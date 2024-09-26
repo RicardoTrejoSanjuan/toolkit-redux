a729483932b476e0bed288b148cf284fc13998b5
RicardoTrejoSanjuan <trejosanjuanricardo@gmail.com>
2024-09-25 19:22:44 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Toolkit Redux
+
+
+npm install react-redux
+npm install @reduxjs/toolkit
+
+
+
+npm install --save @types/react-redux
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <main className="container mx-auto px-6 py-20">
+      <div className="p-8 text-center">
+        <h1 className="text-5xl font-bold text-black mb-4">
+          Welcome to {''}
+          <span className="text-indigo-700">Redux</span>
+        </h1>
+        <p className="text-lg mb-8 text-gray-700">
+          A JS library for predictable and maintainable global state management
+        </p>
+      </div>
+    </main>
+  )
+}
+
+export default App
